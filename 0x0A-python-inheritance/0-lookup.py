6ae8cc2d8bea4628d0af9f5fb0b71c35bab07d85
ahmed <ahmed2004ali2010@gmail.com>
2024-01-08 10:17:11 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''0-lookup.py
+'''
+
+
+def lookup(obj):
+    "st.py
+    '''
+
+
+    class MyList(list):
+        ''' This class inherits from list() '''
+
+            def print_sorted(self):
+                    """ prints the sorted list """
+                            print(sorted(self))
+                            "" returns the list of available attributes and methods of an object """
+    return dir(obj)
